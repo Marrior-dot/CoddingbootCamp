9c2703b9c209e6cdfc27fcefacd55e24a8006adc
Marrior-dot <mrlimaroberto@gmail.com>
2021-05-26 15:39:09 -0300

    função FormValidation() funciona com a aba de nome

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //let em = document.getElementById('email');
-let emValid = /@outlook|gmail|hotmail|yahoo|uol.com$|.com.br$/; 
+/*let emValid = /@outlook|gmail|hotmail|yahoo|uol.com$|.com.br$/; 
 
 function test(validation){
 	if (validation == true){
@@ -14,3 +14,16 @@ console.log(emValid.test("username@gmail.com"));
 console.log(emValid.test("username@hotmail.com"));
 console.log(emValid.test("username@uol.com"));
 console.log(emValid.test("username@gmail.com.br"));
+*/
+function FormValidation(){
+	let nameLabel = document.getElementById('name');
+	let emailLabel = document.getElementById('email').value; 
+	let nameValid = /[a-z]/ig;
+	let emailValid = /\w@gmail|outlook|hotmail|yahoo|uol.com$|.com.br$/
+		if (nameValid.test(nameLabel.value) === true ){
+			return true;
+		} 
+		else {
+			nameLabel.title = 'São aceitas apenas letras maiusculas e minusculas';
+		}
+}
