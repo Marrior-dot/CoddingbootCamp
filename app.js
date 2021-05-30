2d2c9b6fc81edc819d4bddd80d0962bf3a73e67e
Marrior-dot <mrlimaroberto@gmail.com>
2021-05-30 16:10:13 -0300

    regex para nome e email funcionando de maneira correta

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 	let nameLabel = document.getElementById('name');
 	let nameT = document.getElementById('nameTitle');
 	let emailLabel = document.getElementById('email'); 
-
+	let emailT= document.getElementById('emailTitle');
 function FormValidation(){
 	let errorM = [];
-		if (/[^0-9][a-z]/ig.test(nameLabel.value) === false){
-			errorM[0] = (nameLabel.title)	
+	if (/[^0-9][a-z]/ig.test(nameLabel.value) === false){
+			errorM[0] = nameLabel.title	
 			nameT.style.color = '#FA5C6C' 
 			nameT.innerText = errorM[0];			
-			return false;
-		}
-		/*if(/^[0-9a-z]@[a-z]\.[a-z]/ig.test(emailLabel.value) === false){
 			
-		}	
-		*/
+		}
+	if (/^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+\.([a-z]+)\.([a-z])?$/.test(emailLabel.value) === false){
+		return false
+	}
 		
-}
+	console.log(errorM);	
+	if (errorM.length > 0){
+		return false;
+	}
+
+		}
+
